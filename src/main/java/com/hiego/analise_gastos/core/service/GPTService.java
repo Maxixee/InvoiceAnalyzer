f3aed2ec15ee191043ebdae54a130b066d074075
Hiego <hiegobarreto@gmail.com>
2026-07-16 22:34:56 -0300

    Adicionando rate limiter

--- src/main/java/com/hiego/analise_gastos/core/service/GPTService.java
+++ src/main/java/com/hiego/analise_gastos/core/service/GPTService.java
@@ -1,5 +1,7 @@
 package com.hiego.analise_gastos.core.service;
 
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.openai.OpenAiChatOptions;
@@ -15,14 +17,21 @@ public class GPTService {
         this.chatModel = chatModel;
     }
 
+    @RateLimiter(name = "openai")
     public ChatResponse callingGPTapi(String prompt){
-        return chatModel.call(
-                new Prompt(
-                        prompt,
-                        OpenAiChatOptions.builder()
-                                .model("gpt-4o")
-                                .temperature(0.4)
-                                .build()
-                ));
+
+        try{
+            return chatModel.call(
+                    new Prompt(
+                            prompt,
+                            OpenAiChatOptions.builder()
+                                    .model("gpt-4o")
+                                    .temperature(0.4)
+                                    .build()
+                    ));
+        } catch (RequestNotPermitted e) {
+            throw new RuntimeException("Limite de chamadas por minuto atingido. Aguarde alguns instantes.... ", e);
+        }
+
     }
 }
